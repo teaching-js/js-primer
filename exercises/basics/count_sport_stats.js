c6bdfa7f48c832850a44a0f428e5bbf76f7520ad
Alex Hinds <alex.hinds141@gmail.com>
2018-07-20 20:06:48 +1000

    some basic exercise structure

---
+++ exercises/basics/count_sport_stats.js
@@ -0,0 +1,8 @@
+/*
+   Given a list of sports data count the number of
+   matches and tries each player in the list has and return
+   an object containing the sum
+*/
+function count_stats(list) {
+   /* HINT use reduce */
+}
---
+++ exercises/basics/functions.js
@@ -0,0 +1,15 @@
+/* functions can be passed around
+
+   Write a function that executes a list of funcitons, it's
+   passed, in the order they appear.
+
+   And returns the results of each in a list.
+
+   The indexes should match the function list.
+
+*/
+
+function function_dispatcher(function_list, args_list) {
+   /* Your code goes here */
+   return []
+}
