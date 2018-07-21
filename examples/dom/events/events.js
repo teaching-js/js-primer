0409d312d140404303326eb3a646979da3493125
Alex Hinds <alex.hinds141@gmail.com>
2018-07-21 20:33:56 +1000

    minor reformatting of examples

---
+++ examples/dom/events/events.js
@@ -0,0 +1,18 @@
+
+
+function init() {
+
+   const out = document.getElementById('output')
+   window.addEventListener('click', clicker)
+
+   function clicker(event) {
+      out.innerText =
+         `Element Clicked: ${event.target.tagName}\n\
+          Text Content: ${event.target.innerText}`
+   }
+
+   document.removeEventListener('DOMContentLoaded', init)
+}
+
+// here's a differnet way to exec js scripts
+document.addEventListener('DOMContentLoaded', init)
