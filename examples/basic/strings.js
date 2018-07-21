a944dc3b474ca9e4fa5f38ed4000f1895dd36388
Alex Hinds <alex.hinds141@gmail.com>
2018-07-21 19:07:57 +1000

    added a few examples

---
+++ examples/basic/strings.js
@@ -0,0 +1,17 @@
+
+/* String operations */
+let firstName  = "alex"
+let secondName = "afzal"
+
+/* Strings can be concatenated with the + operator */
+const fullName = firstName + " " + secondName
+
+// but maybe we want to capitalise the first letters.
+// turns out to do that it's kinda gross.
+// we can't simply index as we can in other languages
+const properNoun = (noun) =>
+   noun.charAt(0).toUpperCase() + noun.substring(1)
+
+// so to combine that.
+const properFullName =
+   properNoun(firstName) + " " + properNoun(secondName)
