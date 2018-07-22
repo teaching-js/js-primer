418d96a26d55a77621556653faea5de6b903300e
Alex Hinds <alex.hinds141@gmail.com>
2018-07-22 12:19:26 +1000

    fleshed out dict example

--- examples/basic/objects.js
+++ examples/basic/objects.js
@@ -17,3 +17,30 @@ for (const item of cart) {
 
 console.log(count)
 // { Apple: 2, Orange: 2, Strawberry: 1 }
+
+/*
+   As of ES6+ we now also have Map and Set, both of which
+   address one minor, but common issue with using Objects
+   directly as keys. The issue is with more complex keys, JS
+   implicitly uses the toString() method to hash them...
+   Which won't work for Object -> it'll just look like this.
+   [object Object]..
+*/
+
+const shopping_cart = {}
+
+const shopping = [
+   { item: "Apple", price: 10 },
+   { item: "Orange", price: 12 },
+   { item: "Apple", price: 10 }
+]
+
+for (const item of shopping) {
+   // As above.
+   shopping_cart[item] = shopping_cart[item] ? shopping_cart[item] + 1 : 1
+}
+
+console.log(shopping_cart)
+// { '[object Object]': 3 }
+
+// For these use Map or Set instead, depending n what's required.
