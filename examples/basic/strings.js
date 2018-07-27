cf39723e0cf9a0e1f7a5880311bcfc5e126d25d6
Alex Hinds <alex.hinds141@gmail.com>
2018-07-28 08:24:35 +1000

    nahhhh.

--- examples/basic/strings.js
+++ examples/basic/strings.js
@@ -12,9 +12,8 @@ const altFullName = `${firstName} ${secondName}`;
 // but maybe we want to capitalise the first letters.
 // turns out to do that it's kinda gross.
 // we can't simply index as we can in other languages
-const properNoun = (noun) => {
-   return noun.charAt(0).toUpperCase() + noun.substring(1)
- };
+const properNoun = noun => 
+      noun.charAt(0).toUpperCase() + noun.substring(1);
 
 // so to combine that.
 const properFullName =
