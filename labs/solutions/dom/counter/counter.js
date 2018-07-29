aee98b8b092d7209638ce7e0000e29c670a4abd1
Alex Hinds <alex.hinds141@gmail.com>
2018-07-29 15:54:02 +1000

    added more solutions

--- labs/solutions/dom/counter/counter.js
+++ labs/solutions/dom/counter/counter.js
@@ -1,5 +1,12 @@
 (function() {
    'use strict';
-   // write your code here
+   const d = document
+   const output = d.getElementById('output')
+
+   const interval = setInterval(() => {
+      const d = new Date()
+      output.innerText = `${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`
+
+   }, 1000)
 
 }());
--- labs/solutions/dom/toggle/toggle.js
+++ labs/solutions/dom/toggle/toggle.js
@@ -1,4 +1,10 @@
 (function() {
    'use strict';
    // write your js here.
+   const d = document
+   const output = d.getElementById('output')
+
+   const interval = setInterval(() => {
+      output.classList.toggle('hide')
+   }, 2000)
 }());
