5e60a86fa307233f9aa35ef83b9556b2a912d8d4
Alex Hinds <alex.hinds141@gmail.com>
2018-09-20 21:33:26 +1000

    k

--- lectures/week1/dom/dom.js
+++ lectures/week1/dom/dom.js
@@ -4,7 +4,7 @@ const output = document.getElementById('output');
 
 // creating an element
 const p = document.createElement('p');
-p.innerText = "Hello Andrew";
+p.innerText = 'Hello Andrew';
 
 // adding it to the dom tree
 
